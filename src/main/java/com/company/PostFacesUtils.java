a00897dc913fe4b59254243be87624090f3f1562
Mafik2 <73097570+Mafik2@users.noreply.github.com>
2022-11-25 12:03:00 +0100

    Added uuid detail

--- src/main/java/com/company/PostFacesUtils.java
+++ src/main/java/com/company/PostFacesUtils.java
@@ -1,17 +1,28 @@
 package com.company;
 
+import jakarta.enterprise.context.RequestScoped;
 import jakarta.faces.context.FacesContext;
+import jakarta.inject.Inject;
+import jakarta.inject.Named;
 
 import java.io.IOException;
 import java.util.Map;
-
+@RequestScoped
+@Named
 public class PostFacesUtils {
+    private Post post;
+    @Inject
+    private PostService postService;
+
     public void redirect() throws IOException {
         FacesContext.getCurrentInstance().getExternalContext().redirect("index.xhtml");
     }
     public String getParam(){
         Map<String, String> params = FacesContext.getCurrentInstance().getExternalContext().getRequestParameterMap();
-
         return params.get("id");
+    }
+    public Post getPost() {
+        this.post = this.postService.getById(getParam());
+        return post;
     }
 }
